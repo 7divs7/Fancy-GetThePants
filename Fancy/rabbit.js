dc1caaf34816a2c93e9afc3fe7ff6fe4287af87d
Divyani Panda <divyani.panda@gmail.com>
2020-05-16 11:34:49 +0530

    Add files via upload

---
+++ Fancy/rabbit.js
@@ -0,0 +1,393 @@
+class Game
+{
+    constructor(display_width, display_height)
+    {
+        this.display_width = display_width;
+        this.display_height = display_height;
+    }
+
+    start()
+    {
+        this.player = new Player(this.display_width, this.display_height);
+        
+        this.platforms = [];
+
+        for(let i = -1; i < 4; i++)
+        {
+            let pos = (Math.random()*200) + 200;
+            this.platforms.push(new Platform(pos,0 + 200*i, '#0f0',this.display_width, this.display_height));
+        }
+       
+        
+        
+        new InputHandler(this.player);
+
+        
+    }
+    
+    
+    
+
+    update(deltaTime)
+    {   
+        
+        this.platforms.forEach((object) => object.update(deltaTime));
+        this.player.update(deltaTime);
+        collision(this.player, this.platforms);
+        
+        this.platforms.forEach((object,index1) => {
+            if(object.y > 570)
+            {
+                this.platforms.splice(index1,1);
+                let pos = (Math.random()*200) + 200;
+                
+                this.platforms.push(new Platform(pos,-400, '#0f0',this.display_width, this.display_height));
+                this.player.count += 1;
+                console.log(this.player.count);
+            }
+        });
+        
+    }
+
+    draw(ctx)
+    {
+        if(this.player.count < 3)
+        {
+            draw_base();
+        }
+
+        //this.platforms.forEach((object) => object.draw(ctx));
+            
+        this.platforms.forEach((object, index) => {
+            if(this.player.count == 15 && index == 2) 
+            {
+                object.draw_pants(ctx);
+                object.color = '#00f';
+                //this.shit = true;
+                //platforms = platforms.filter(object1 => object.shit);
+            }
+            else{
+                object.draw(ctx);
+                //this.shit = true;
+            }
+        })
+        
+        //this.platform.draw(ctx);
+        this.player.draw(ctx);
+    }
+}
+
+class InputHandler
+{
+    constructor(player)
+    {
+        document.addEventListener("keydown", event => {
+            //alert(event.keyCode);
+            switch(event.keyCode)
+            {
+                case 37: player.move_left();
+                break;
+                case 39: player.move_right();
+                break;
+                case 32: player.jump();
+                break;
+            }
+        });
+        
+        document.addEventListener("keyup", event => {
+            //alert(event.keyCode);
+            switch(event.keyCode)
+            {
+                case 37: if(player.x_vel<0) player.stop();
+                break;
+                case 39: if(player.x_vel>0) player.stop();
+                break;
+                case 32: player.dont_jump();
+            }
+        });
+        
+    }
+}
+
+
+class Player
+{
+    constructor(display_width, display_height)
+    {
+        this.image = document.getElementById("boy");
+
+        this.t = false;
+        this.count = 0;
+
+        this.display_width = display_width;
+        this.display_height = display_height;
+
+        this.width = 45;
+        this.height = 60;
+
+        this.x_vel = 0;
+        this.y_vel = 0;
+
+        this.jumping = false;
+        
+        this.position = {
+            //x: display_width/2 - this.width/2,
+            //y: display_height - this.height-10
+            x: 30,
+            y: 0
+        };
+        
+        this.poy = 0;
+        this.pox = 0;
+    }
+    
+    draw(ctx)
+    {
+        //ctx.fillStyle = '#f00';
+        //ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+    }
+
+    move_left()
+    {
+        this.x_vel = -5;
+        
+    }
+
+    move_right()
+    {
+       this.x_vel = 5;
+       
+    }
+
+    stop()
+    {
+        this.x_vel = 0;
+        //this.x_vel -= this.x_vel*0.2; //friction
+    
+    }
+
+    jump()
+    {
+        this.t = true;
+        /*
+        if(this.jumping == false)
+        {   
+            //console.log("geygd");
+            this.y_vel -=40;
+            this.jumping = true;
+        }
+        */
+    }
+    dont_jump()
+    {
+        this.t = false;
+    }
+
+    update(deltaTime)
+    {
+        if(this.t && this.jumping == false)
+        {
+            //console.log("geygd");
+            this.y_vel -=35;
+            this.jumping = true;
+        }
+
+        //this.jumping = true;
+
+        this.y_vel += 1.5; //gravity
+
+        this.pox = this.position.x; // old x pos
+        this.poy = this.position.y; // old y pos
+        this.position.x += this.x_vel;
+        this.position.y += this.y_vel; 
+        this.y_vel *= 0.95; //friction
+
+        //falling below floor
+        if(this.position.y > 520 && this.count <= 2)
+        {
+            this.jumping = false;
+            this.position.y = 520;
+            this.y_vel = 0;
+        }
+        
+        if(this.position.y > 520 && this.count > 2)
+        {
+            //alert("gameover");
+            window.location.href = "plat_play.html";
+        }
+
+        //check boundaries
+        
+        if(this.position.x < 0)
+        {
+            this.x_vel = 0;
+            this.position.x = 0;
+        }
+        if(this.position.x + this.width > this.display_width)
+        {
+            this.x_vel = 0;
+            this.position.x = this.display_width - this.width;
+        }
+        
+        
+        
+        
+    }
+}
+
+class Platform
+{
+    constructor(x,y,color,display_width, display_height)
+    {
+        this.image = document.getElementById("platpic");
+        this.image2 = document.getElementById("pants");
+
+        this.x = x;
+        this.y = y;
+        this.rotation = 0;
+        this.width = 95;
+        this.height = 25;
+        this.vx = 0;
+        this.vy = 0;
+        this.ox = 0;
+        this.oy = 0;
+        this.color = color;
+
+        this.display_height = display_height;
+        this.display_width = display_width;
+    }
+    
+    moveX()
+    {
+        
+
+        this.ox = this.x;
+        this.oy = this.y;
+        //this.x = this.x + Math.cos(this.rotation);
+        this.vx = this.x + Math.cos(this.rotation) - this.x;
+        this.x += this.vx;
+    }
+
+    moveY()
+    {
+        this.rotation += 0.01;
+        this.ox = this.x;
+        this.vx = this.x + Math.cos(this.rotation) - this.x;
+        this.x += this.vx;
+
+        this.oy = this.y; 
+        //this.vy = this.y + Math.cos(this.rotation) - this.y;
+        this.vy = 0.5;
+        this.y += this.vy;
+        //this.y = this.y % display_height;
+    }
+
+    moveXY()
+    {
+        this.rotation += 0.01;
+
+        this.ox = this.x;
+        this.vx = this.x + Math.cos(this.rotation) - this.x;
+        this.oy = this.y;
+        this.vy = this.y + Math.cos(this.rotation) - this.y;
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+
+    update(deltaTime)
+    {
+        this.moveY();
+    }
+    
+    draw(ctx)
+    {
+        //ctx.fillStyle = this.color;
+        //ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+    draw_pants(ctx)
+    {
+        ctx.drawImage(this.image2, this.x, this.y-50, 50, 50);
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+}
+
+
+function collision(player, platforms)
+{
+    //for(let i = platforms.length - 1; i > -1; --i)
+    for(let i = 0; i<platforms.length; i++)
+    {
+        let platform = platforms[i];
+
+        
+        if(player.position.x + player.width/2 > platform.x && player.position.x < platform.x + platform.width)
+        {
+            if(player.position.y + player.height > platform.y &&
+                player.poy + player.height <= platform.oy)
+            {
+                
+                player.position.y = platform.y - player.height;
+                player.y_vel = 0;
+                player.jumping = false;
+                player.x_vel += (platform.vx - player.x_vel)*0.1;
+                if(platform.color == '#00f')
+                {
+                    //alert("you won");
+                    window.location.href = "plat_play.html";
+                }
+            }
+        }
+            
+    }
+}
+
+function draw_base()
+{
+  ctx.drawImage(grass_img,0,520, display_width,80); 
+}
+//////////////////////////////////////
+//////////////////////////////////////////
+///////////////////////////////////////////////
+//////////////////////////////////////
+//////////////////////////////////////////
+///////////////////////////////////////////////
+
+
+
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");  //to fill in the canvas ctx is your screen
+let cloud_img = document.getElementById("clouds");
+let grass_img = document.getElementById("grass");
+
+const display_width = 800;
+const display_height = 600;
+
+
+let game = new Game(display_width, display_height);
+game.start();
+
+let lastTime = 0;
+
+function gameLoop(timestamp)
+{
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
+    //ctx.clearRect(0,0,display_width,display_height);
+    ctx.drawImage(cloud_img,0,0, display_width,display_height); 
+    //ctx.clearRect(0,0,);
+
+    //BASE
+    
+
+
+
+    game.update(deltaTime);
+    game.draw(ctx);
+
+    requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop); 
---
+++ Fancy/rabbit_play.js
@@ -0,0 +1,74 @@
+
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");  //to fill in the canvas ctx is your screen
+let curtain_img = document.getElementById("curtain");
+let play_img = document.getElementById("play_again");
+let boy_img = document.getElementById("boy");
+let pants_img = document.getElementById("pants");
+
+
+canvas.addEventListener("click", onCanvasClick, false);
+
+  function onCanvasClick(e) {
+      var x; 
+      var y;
+      [x,y] = getCursorPosition(e);
+      if(x >= 289 && x<= 476 && y>= 340 && y<= 420)
+      {
+        window.location.href = "plat.html";
+      }
+  }
+
+  function getCursorPosition(e) {
+  	var x;
+    var y;
+    if (e.pageX != undefined && e.pageY != undefined) {
+	x = e.pageX;
+	y = e.pageY;
+    }
+    else {
+	x = e.clientX + document.body.scrollLeft +
+            document.documentElement.scrollLeft;
+	y = e.clientY + document.body.scrollTop +
+            document.documentElement.scrollTop;
+    }
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+    
+    //window.location.href = "plat.html";
+
+return [x,y];
+  }
+
+
+const display_width = 800;
+const display_height = 600;
+
+
+
+let lastTime = 0;
+
+function gameLoop(timestamp)
+{
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
+    ctx.drawImage(curtain_img,0,0, display_width,display_height); 
+    ctx.drawImage(play_img,display_width/2 - 120,display_height/2 + 30 , 200, 100);
+    ctx.drawImage(boy_img,0,40, 150, 200);
+    ctx.drawImage(pants_img,630,15, 100, 100); 
+   
+
+    requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop); 
+
+
+
+
+
+
+    
+
+
